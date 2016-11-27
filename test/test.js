59402b34d1b9f5fff05b9a69ac37f80ac57a180e
Kristoffer Gunnarsson <kristoffergunnarsson47@gmail.com>
2016-11-27 14:46:43 +0100

    Added test cases for different criteria combinations
    The actual and possible grades generated by the criteria combinations
    may need some revision

--- test/test.js
+++ test/test.js
@@ -11,14 +11,77 @@ describe("grading", function() {
       assert.deepEqual(require("../lib.js").compulsory.grading, grading);
     });
   });
+  
   /*
     Keys are the input (will split into an array of grades)
     Values are the output (first letter = result.guaranteed, second letter = result.possible).
       If either char is "n", the expected value of the corresponding result entry is false.
   */
   const tests = {
+
+    "AAAAA": "An",
+    "AAAA": "An",
     "AAA": "An",
-    "AAA": "An",
+
+    "CCCCC": "Cn",
+    "CCCC": "Cn",
+    "CCC": "Cn",
+
+    "EEEEE": "En",
+    "EEEE": "En",
+    "EEE": "En",
+
+    "AAAAC": "BA",
+    "AAACC": "BA",
+    "AAAC": "BA",
+    "AAC": "BA",
+
+    "AACCC": "CB",
+    "AACC": "CB",
+    "ACCC": "CB",
+    "ACCCC": "CB",
+    "ACC": "CB",
+
+    "AAEEE": "ED",
+    "ACEEE": "ED",
+    "AEEEE": "ED",
+    "AAEE": "ED",
+    "AEEE": "ED",
+    "CCEEE": "ED",
+    "CEEEE": "ED",
+    "CCEE": "ED",
+    "CEEE": "ED",
+    "CEE": "ED",
+
+    "AAAAE": "DC",
+    "AAACE": "DC",
+    "AAAEE": "DC",
+    "AACCE": "DC",
+    "AACEE": "DC",
+    "ACCCE": "DC",
+    "ACCEE": "DC",
+    "AAAE": "DC",
+    "AACE": "DC",
+    "ACCE": "DC",
+    "ACEE": "DC",
+    "CCCCE": "DC",
+    "CCCEE": "DC",
+    "CCCE": "DC",
+    "ACE": "DC",
+    "AEE": "DC",
+    "AAE": "DC",
+    "CCE": "DC",
+
+    "AAF": "Fn",
+    "ACF": "Fn",
+    "AEF": "Fn",
+    "AFF": "Fn",
+    "CCF": "Fn",
+    "CEF": "Fn",
+    "CFF": "Fn",
+    "EEF": "Fn",
+    "EFF": "Fn",
+    "FFF": "Fn"
   };
 
   for(let inputGrades in tests) {
